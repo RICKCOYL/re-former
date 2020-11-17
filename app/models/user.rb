3974e6ca3d855759ee246b1189d9d885f7c0de99
RICKCOYL <rickoburu@gmail.com>
2020-11-17 17:06:29 +0300

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: {maximum: 50}
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, uniqueness: true, length: {maximum: 50}
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true, length: { maximum: 50 }
 end
