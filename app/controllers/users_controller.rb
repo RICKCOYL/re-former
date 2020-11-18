c5a5813fa440d526af07a4a8a97b120b0a7ea6f7
RICKCOYL <rickoburu@gmail.com>
2020-11-18 19:46:59 +0300

    fix tse recommended changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
 
     if @user.save
@@ -12,9 +11,7 @@ class UsersController < ApplicationController
       redirect_to new_user_path
 
     else
-
       render :new
-
     end
   end
 
