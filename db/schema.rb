04aa940682e7a84fcd61fe926f44bda3101c7d7a
RICKCOYL <rickoburu@gmail.com>
2020-11-18 15:33:56 +0300

    fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -9,15 +9,15 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+# rubocop:disable Style/NumericLiterals
 
-ActiveRecord::Schema.define(version: 2020_11_16_132743) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_201_116_132_743) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
+# rubocop:disable Style/NumericLiterals
